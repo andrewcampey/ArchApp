b5571549c604eb0c1df0d006def6ccab3d6fb466
Andrew Campey <andrew.campey@gmail.com>
2012-09-29 09:53:21 +1000

    Setup 'home' page routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-ArchApp::Application.routes.draw do
+ArchApp::Application.routes.draw do |map|
   get "home/home"
-  
-	map.root :controller => "home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -59,4 +57,6 @@ ArchApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  map.root :controller => "home"  
 end
