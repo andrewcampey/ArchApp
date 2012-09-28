0a4df78829c8e8261fc3931d0f98d186fee12d6e
Andrew Campey <andrew.campey@gmail.com>
2012-09-29 09:03:50 +1000

    Created new test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArchApp::Application.config.secret_token = '674d04e253de39c3ee49c4b5a82b6e35a585f7a927688bc0d31c8da92d4d24be87b26565b3fb9f232abaf7d9592650a9bcf3ad4111b37fe4e1403cf462054806'
