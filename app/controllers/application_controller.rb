81168199156dc034492524fc5b3667e4260ecf15
Andrew Campey <andrew.campey@gmail.com>
2012-10-03 20:32:23 +1000

    Attempting to get login work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   include LoginSystem
-
+  helper_method :current_user, :prefs
   
   prepend_before_filter :login_required
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+	before_filter :login_from_cookie
   def home
   end
 end
